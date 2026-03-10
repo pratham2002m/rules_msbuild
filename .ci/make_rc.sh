769c5456e00ac766253178120f673c54eff649a3
pramor6z <prathamesh.more@siemens.com>
2026-03-10 19:14:49 +0530

    feat: enable net10.0

---
+++ .ci/make_rc.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+./.buildbuddy/make_rc.sh
+
+cat >>.bazelrc <<EOF
+build --announce_rc
+build --verbose_failures
+test --test_output=errors
+#test --test_env=GO_TEST_WRAP_TESTV=1 # otherwise go doesn't report tests that pass
+try-import %workspace%/buildbuddy.bazelrc
+EOF
